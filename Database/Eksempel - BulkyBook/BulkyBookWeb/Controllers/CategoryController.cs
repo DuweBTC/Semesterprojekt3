8fa2773c3c1a52f19d413756f7d7f8d04ed50b99
BjarkeKarlsen <37935745+BjarkeKarlsen@users.noreply.github.com>
2022-04-13 14:06:18 +0200

    Eksempel på database på PC

---
+++ Database/Eksempel - BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,122 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db) // Initialization 
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories; // Create a ?
+            return View(objCategoryList);       // Link it to Views (Index.cshtml)
+        }
+
+        //GET Action methode
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST Action methode
+        [HttpPost]                              //These keys need to be here
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);            // Add object to the database 
+                _db.SaveChanges();                  // It first update the database here
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");  // Go Back to index site
+            }
+            return View(obj);
+        }
+
+
+        //GET Action methode
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //POST Action methode
+        [HttpPost]                              //These keys need to be here
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);            // Add object to the database 
+                _db.SaveChanges();                  // It first update the database here
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");  // Go Back to index site
+            }
+            return View(obj);
+        }
+
+
+        //GET Action methode
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //POST Action methode
+        [HttpPost,ActionName("Delete")]                              //These keys need to be here
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(obj);            // Add object to the database 
+            _db.SaveChanges();                  // It first update the database here
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");  // Go Back to index site
+           
+        }
+    }
+}
---
+++ Database/Eksempel - BulkyBook/BulkyBookWeb/obj/Release/net6.0/win-x86/PubTmp/EFSQLScripts/BulkyBookWeb.Data.ApplicationDbContext.sql
@@ -0,0 +1,35 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220412094356_AddCategoryToDatebase')
+BEGIN
+    CREATE TABLE [Categories] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NOT NULL,
+        [DisplayOrder] int NOT NULL,
+        [CreatedDateTime] datetime2 NOT NULL,
+        CONSTRAINT [PK_Categories] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220412094356_AddCategoryToDatebase')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20220412094356_AddCategoryToDatebase', N'6.0.3');
+END;
+GO
+
+COMMIT;
+GO
+
