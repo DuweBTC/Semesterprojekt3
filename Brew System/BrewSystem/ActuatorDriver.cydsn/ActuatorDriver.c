0c35e9a9198f67586a05249aff1e57aa2d0b7cc1
KasperMartensen <tigriss.tiger@gmail.com>
2022-05-16 11:56:07 +0200

    Startet på ActuatorDriver implemntering

---
+++ Brew System/BrewSystem/ActuatorDriver.cydsn/ActuatorDriver.c
@@ -0,0 +1,31 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+#include "ActuatorDriver.h"
+
+void openValveNr(int valveNumber)
+{
+    switch(valveNumber)
+    {
+        
+    }
+}
+
+void closeValveNr(int valveNumber)
+{
+    switch(valveNumber)
+    {
+        
+    }   
+}
+
+/* [] END OF FILE */
---
+++ Brew System/BrewSystem/ActuatorDriver.cydsn/ActuatorDriver.h
@@ -0,0 +1,23 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+#pragma once
+#include "project.h"
+
+/* Public Functions */
+void openValveNr(int valveNumber);
+void closeValveNr(int valveNumber);
+
+/* Private Functions */
+
+
+/* [] END OF FILE */
