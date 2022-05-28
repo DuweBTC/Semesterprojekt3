28861f171b28e202df5becf1fd3ede3cd064d46d
BjarkeKarlsen <37935745+BjarkeKarlsen@users.noreply.github.com>
2022-05-28 22:21:49 +0200

    second_try

---
+++ Database/ServerWebApi/Migrations/Recipe/20220528202045_AddRecipeToDatabase_v1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServerWebApi.Migrations.Recipe
+{
+    public partial class AddRecipeToDatabase_v1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
